a4508bc7e8c5c7c9cb57eeea958b24312aebfbfa
Eric Leblond <eric@regit.org>
2012-06-03 12:41:53 +0200

    Renaming and initial version of setup.py.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='opensvp',
+      version='0.1',
+      description='Application layer gateway testing tool',
+      author='Eric Leblond',
+      author_email='eric@regit.org',
+      url='https://home.regit.org/software/opensvp/',
+      scripts=['opensvp'],
+      packages=['opensvp'],
+      package_dir={'opensvp':'lib/opensvp'},
+      provides=['opensvp'],
+      requires=['argparse', 'scapy', 'ftplib'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: System :: Networking :: Firewalls',
+          ],
+      )
