f7b9be78e42bc41d1e13b2024a9b47df0e4d90ff
Eric Leblond <eric@regit.org>
2012-06-03 15:32:37 +0200

    Move dir and upgrade version.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 from setuptools import setup
 
 setup(name='opensvp',
-      version='0.2',
+      version='0.3',
       description='Application layer gateway testing tool',
       author='Eric Leblond',
       author_email='eric@regit.org',
       url='https://home.regit.org/software/opensvp/',
       scripts=['opensvp'],
       packages=['opensvp'],
-      package_dir={'opensvp':'lib/'},
+      package_dir={'opensvp':'src'},
       provides=['opensvp'],
       requires=['argparse', 'scapy', 'ftplib'],
       classifiers=[
